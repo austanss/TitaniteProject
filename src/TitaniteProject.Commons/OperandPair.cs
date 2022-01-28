ff9053356bc52fe6e3b424ead31e118b9cb3b6ed
austanss <ryzenupp@hotmail.com>
2022-01-28 15:18:19 -0500

    Relocate project commons

---
+++ src/TitaniteProject.Commons/OperandPair.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TitaniteProject.Commons
+{
+    internal struct OperandPair
+    {
+        public OperandPair(ulong left, ulong right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public readonly ulong Left;
+        public readonly ulong Right;
+
+        public ulong this[int index]
+        {
+            get
+            {
+                return index switch
+                {
+                    0 => Left,
+                    1 => Right,
+                    _ => throw new IndexOutOfRangeException()
+                };
+            }
+        }
+    }
+}
